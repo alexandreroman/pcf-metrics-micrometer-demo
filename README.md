2f927d50e574704a8053aaa31d7be97678d416ba
Alexandre Roman <aroman@pivotal.io>
2019-01-13 19:23:45 +0100

    Init project

---
+++ README.md
@@ -0,0 +1,74 @@
+# PCF Metrics & Micrometer demo
+
+This project shows how to monitor a Spring Boot app with
+[PCF Metrics](https://pivotal.io/platform/services-marketplace/monitoring-metrics-and-logging/pcf-metrics)
+and [Micrometer](https://micrometer.io).
+
+## How to use it?
+
+Compile this project using a JDK 8:
+```bash
+$ ./mvnw clean package
+```
+
+Run this app:
+```bash
+$ java -jar target/pcf-metrics-micrometer-demo.jar
+```
+
+This app is exposing a single endpoint at http://localhost:8080:
+```bash
+$ curl http://localhost:8080
+Hello world%
+```
+
+Each time you access this endpoint, a counter is incremented.
+This counter is exposed using a Spring Boot Actuator endpoint, available at http://localhost:8080/actuator/accesslog:
+```bash
+$ curl http://localhost:8080/actuator/accesslog
+{"counter":1}%
+```
+
+You can reset this counter by sending a `DELETE` request to this endpoint:
+```bash
+$ curl -X DELETE http://localhost:8080/actuator/accesslog
+$ curl http://localhost:8080/actuator/accesslog
+{"counter":0}%
+```
+
+## Use PCF Metrics to get app metrics
+
+Create a [PCF Metrics Forwarder](https://docs.pivotal.io/metrics-forwarder) service instance:
+```bash
+$ cf create-service metrics-forwarder unlimited metrics-forwarder
+```
+
+Push this app to PCF:
+```bash
+$ cf push
+```
+
+Bind your app to the metrics forwarder instance, and reload it:
+```bash
+$ cf bind-service pcf-metrics-micrometer-demo metrics-forwarder
+$ cf restage pcf-metrics-micrometer-demo
+```
+
+This app is now exporting its metrics to PCF Metrics.
+You can create a metric chart to show these values on the PCF Metrics Dashboard.
+<img src="https://imgur.com/download/S9j99sE"/>
+
+You can also set up a webhook. For example, you can send a message to a Slack channel if the value is too high:
+<img src="https://imgur.com/download/e53KA7e"/>
+
+## Contribute
+
+Contributions are always welcome!
+
+Feel free to open issues & send PR.
+
+## License
+
+Copyright &copy; 2019 [Pivotal Software, Inc](https://pivotal.io).
+
+This project is licensed under the [Apache Software License version 2.0](https://www.apache.org/licenses/LICENSE-2.0).
---
+++ src/main/java/fr/alexandreroman/demos/pcfmetricsmicrometerdemo/Application.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) 2019 Pivotal Software, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fr.alexandreroman.demos.pcfmetricsmicrometerdemo;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.endpoint.annotation.DeleteOperation;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
+@Controller
+@RequiredArgsConstructor
+class HelloController {
+    private final AccessLog accessLog;
+
+    @GetMapping("/")
+    @ResponseBody
+    public String hello() {
+        accessLog.incrementCounter();
+        return "Hello world";
+    }
+}
+
+/**
+ * Business component holding metric values.
+ */
+@Component
+class AccessLog {
+    private final AtomicLong counter = new AtomicLong();
+
+    public void incrementCounter() {
+        counter.incrementAndGet();
+    }
+
+    public long getCounter() {
+        return counter.get();
+    }
+
+    public void resetCounter() {
+        counter.set(0);
+    }
+}
+
+/**
+ * Export metrics to a Spring Boot actuator endpoint.
+ */
+@Component
+@Endpoint(id = "accesslog")
+@RequiredArgsConstructor
+class AccessLogActuator {
+    private final AccessLog accessLog;
+
+    @ReadOperation
+    public AccessLogActuatorValues get() {
+        return new AccessLogActuatorValues(accessLog.getCounter());
+    }
+
+    @DeleteOperation
+    public void reset() {
+        accessLog.resetCounter();
+    }
+}
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+class AccessLogActuatorValues {
+    private final long counter;
+}
+
+/**
+ * Export metrics to Micrometer.
+ */
+@Configuration
+@RequiredArgsConstructor
+class AccessLogMicrometer {
+    private final AccessLog accessLog;
+
+    @Bean
+    public Gauge accessLogCounter(MeterRegistry registry) {
+        return Gauge.builder("accesslog.counter", () -> accessLog.getCounter())
+                .tag("kind", "performance")
+                .description("Service access count")
+                .register(registry);
+    }
+}
